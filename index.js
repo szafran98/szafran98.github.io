483d10ecbf22b0c98c77a9d5739f2f8931e6319c
szafran98 <szafran1308@gmail.com>
2020-02-06 22:39:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,136 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 350,
+        "density": {
+          "enable": true,
+          "value_area": 789.1476416322727
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 200,
+          "height": 50
+        }
+      },
+      "opacity": {
+        "value": 0.48927153781200905,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 0.1,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 2,
+          "size_min": 0,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 83.91608391608392,
+          "size": 1,
+          "duration": 3,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": false
+  });
+
+
+
+var cards = document.querySelectorAll(".card");
+var playing = false;
+
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('click',function() {
+    if(playing)
+      return;
+    
+    playing = true;
+    anime({
+      targets: this,
+      scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+      rotateY: {value: '+=180', delay: 200},
+      easing: 'easeInOutSine',
+      duration: 400,
+      complete: function(anim){
+         playing = false;
+      }
+    });
+  });
+}
+
+
