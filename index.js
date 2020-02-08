727f438c89c6dff3aab87ba7fca1878d15f87637
szafran98 <szafran1308@gmail.com>
2020-02-08 18:31:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -113,24 +113,75 @@ particlesJS("particles-js", {
 
 var cards = document.querySelectorAll(".card");
 var playing = false;
+var animated = false;
 
 for (var i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click',function() {
     if(playing)
       return;
-    
-    playing = true;
-    anime({
-      targets: this,
-      scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
-      rotateY: {value: '+=180', delay: 200},
-      easing: 'easeInOutSine',
-      duration: 400,
-      complete: function(anim){
-         playing = false;
-      }
-    });
+    if (animated === false) {
+      playing = true;
+      anime({
+        targets: cards,
+        scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+        rotateY: {value: '+=180', delay: 200},
+        easing: 'easeInOutSine',
+        duration: 600,
+        complete: function(anime){
+          playing = false;
+          animated = true;
+          $(".card").addClass("shadow-lg");
+        }
+      });
+    }
   });
 }
 
 
+var docY;
+$(document).scroll(function() {
+  docY = window.scrollY;
+  console.log(docY);
+  if (docY > 2000 && docY < 2200) {
+    $(".card").trigger("click");
+    //$(".card").addClass("shadow-lg");
+  }
+})
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            
+          };
+        });
+      }
+    }
+  });
